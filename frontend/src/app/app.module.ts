f1ed7b795ae4261448906bf366037c82b95ccf39
Kolia Parasochka <koliaparasochka99@gmail.com>
2021-09-11 15:22:23 +0300

    Adds header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { HeaderComponent } from './modules/header/header.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavContainer, MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule
   ],
-  providers: [],
+  providers: [MatSidenavContainer],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
